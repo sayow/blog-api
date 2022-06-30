195159861060c70728386f3aba6d4e51e75ad65c
Sayonara Silva <sayow@hotmail.com>
2022-06-30 16:37:39 +0100

    schema e models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_103320) do
+ActiveRecord::Schema.define(version: 2022_06_30_150457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2022_06_30_103320) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "posts_id", null: false
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -28,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_06_30_103320) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "comment_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -47,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_06_30_103320) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts", column: "posts_id"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "subcomments", "comments"
