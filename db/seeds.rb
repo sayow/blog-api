2b91be31b05b3620dd634be3f87ad9df32939bc9
Sayonara Silva <sayow@hotmail.com>
2022-06-30 21:33:21 +0100

    seeds populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Destroying old data...........................'
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+Subcomment.destroy_all
+
+# _________________________________________________________________
+
+puts 'Creating new users..........................'
+10.times do
+  User.create(name: Faker::FunnyName.name)
+end
+
+# _________________________________________________________________
+
+puts 'Writing new posts..........................'
+users = User.all
+10.times do
+  Post.create(post_content: Faker::Lorem.paragraph, user: users.sample)
+end
+
+# _________________________________________________________________
+
+puts 'Commenting the posts..........................'
+
+10.times do
+  Comment.create(comment_content: Faker::Lorem.sentences, user: users.sample)
+end
+
+# _________________________________________________________________
+
+puts 'Commenting the coments..........................'
+comments = Comment.all
+10.times do
+  Subcomment.create(subcomment_content: Faker::Lorem.question, user: users.sample, comment: comments.sample)
+end
