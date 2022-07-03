ca9c67614c37cd929c67efe3b337dc1bf3f9c7ae
Sayonara Silva <sayow@hotmail.com>
2022-07-03 14:00:50 +0100

    finding show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'homepage' => 'homepages#home'
+  root to: 'posts#home', as: :list_of_posts
+  get 'show/:id' => 'posts#show'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
