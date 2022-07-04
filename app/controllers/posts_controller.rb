344e2810c27a0927b9852bb906b03b4f59736809
Sayonara Silva <sayow@hotmail.com>
2022-07-04 13:01:41 +0100

    config new page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,44 @@ require 'json'
 require 'open-uri'
 
 class PostsController < ApplicationController
+  before_action :set_get_post, only: [:show, :edit, :update, :destroy]
 
-  def home
+  def index
     @get_posts = get_posts?
+    @get_posts
   end
 
   def show
-    @get_posts = get_posts?
-    @post = @post.find(params[:id])
   end
 
+  def new
+    @new_post = Post.new
+  end
+
+  def create
+    @new_post = Post.new(get_post_params)
+    if @get_post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @get_post.update(get_post_params)
+      redirect_to home_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @get_post.destroy
+    redirect_to home_path
+  end
 
   private
 
@@ -20,5 +48,14 @@ class PostsController < ApplicationController
     posts = URI.open(url).read
     @post = JSON.parse(posts)
   end
+
+  def get_post_params
+    params.require(:get_post).permit(:post_content, :user_id, :id)
+  end
+
+  def set_get_post
+    @get_post = Post.find(params[:id])
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'posts#home'
-  get '/show/:id', to: 'posts#show'
+  get    "posts",          to: "posts#index"
+  get    "posts/new",      to: "posts#new",  as: :new_post
+  post   "posts",          to: "posts#create"
+  get    "posts/:id",      to: "posts#show", as: :post
+  get    "posts/:id/edit", to: "posts#edit", as: :edit_post
+  patch  "posts/:id",      to: "posts#update"
+  delete "posts/:id",      to: "posts#destroy"
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
